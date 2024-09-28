448ea033ad1be2995a8e65d369b8a437bd7162ad
Shaikh Mohammad Umar Farooq <shaikhumar06122001@gmail.com>
2024-09-28 22:16:44 +0530

    all desktop screens responsiveness complete

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const footerElement = document.querySelector(".section-footer");
 
 const scrollElement = document.createElement("div");
 scrollElement.classList.add("scrollTop-style", "arrow-down-translate");
-scrollElement.innerHTML = `<i class="fa-solid fa-arrow-down" id="scrollFloatingButton"></i>`;
+scrollElement.innerHTML = `<i class="fa-solid fa-arrow-up" id="scrollFloatingButton"></i>`;
 
 footerElement.after(scrollElement);
 
@@ -14,33 +14,34 @@ const scrollTop = () => {
     headerSection.scrollIntoView({ behavior: "smooth" });
 };
 
-const scrollToAbout = () => {
-    aboutSection.scrollIntoView({ behavior: "smooth" });
-};
+// const scrollToAbout = () => {
+//     aboutSection.scrollIntoView({ behavior: "smooth" });
+// };
 
-scrollElement.addEventListener("click", scrollToAbout);
+// scrollElement.addEventListener("click", scrollToAbout);
+scrollElement.addEventListener("click", scrollTop);
 
-window.addEventListener("scroll", () => {
-    const scrollPosition = window.scrollY;
-    const heroHeight = heroSection.offsetHeight;
+// window.addEventListener("scroll", () => {
+//     const scrollPosition = window.scrollY;
+//     const heroHeight = heroSection.offsetHeight;
 
-    const scrollFloatingButton = document.getElementById(
-        "scrollFloatingButton"
-    );
+//     const scrollFloatingButton = document.getElementById(
+//         "scrollFloatingButton"
+//     );
 
-    if (scrollPosition < heroHeight) {
-        scrollFloatingButton.classList.remove("fa-arrow-up");
-        scrollFloatingButton.classList.add("fa-arrow-down");
-        scrollElement.classList.add("arrow-down-translate");
-        scrollElement.addEventListener("click", scrollToAbout);
-    } else {
-        scrollFloatingButton.classList.remove("fa-arrow-down");
-        scrollElement.classList.remove("arrow-down-translate");
+//     if (scrollPosition < heroHeight) {
+//         scrollFloatingButton.classList.remove("fa-arrow-up");
+//         scrollFloatingButton.classList.add("fa-arrow-down");
+//         scrollElement.classList.add("arrow-down-translate");
+//         scrollElement.addEventListener("click", scrollToAbout);
+//     } else {
+//         scrollFloatingButton.classList.remove("fa-arrow-down");
+//         scrollElement.classList.remove("arrow-down-translate");
 
-        scrollFloatingButton.classList.add("fa-arrow-up");
-        scrollElement.addEventListener("click", scrollTop);
-    }
-});
+//         scrollFloatingButton.classList.add("fa-arrow-up");
+//         scrollElement.addEventListener("click", scrollTop);
+//     }
+// });
 //* SCROLL TO TOP BUTTON end
 
 //* ACTIVE CLASS RESPONSIVE NAV start
@@ -58,7 +59,17 @@ const toggleMenuButton = () => {
 menuButton.addEventListener("click", toggleMenuButton);
 
 navListButtons.forEach((navListButton) => {
-    navListButton.addEventListener("click", toggleMenuButton);
+    navListButton.addEventListener("click", (e) => {
+        headerSection.classList.remove("active");
+        document.documentElement.classList.remove("no-scroll-touch");
+        document.body.classList.remove("no-scroll-touch");
+
+        // const clickedElementName = e.target.name;
+        // const clickedElement = document.getElementById(clickedElementName);
+        // clickedElement.scrollIntoView({ behavior: "smooth" });
+
+        // console.log(clickedElement);
+    });
 });
 
 //* ACTIVE CLASS RESPONSIVE NAV end
