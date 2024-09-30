96f81baa583b9b5a2585b1959ffbbc0d0f87a1bc
Shaikh Mohammad Umar Farooq <shaikhumar06122001@gmail.com>
2024-09-30 21:31:51 +0530

    all mobile responsiveness complete

--- index.js
+++ index.js
@@ -6,42 +6,9 @@ const footerElement = document.querySelector(".section-footer");
 
 const scrollElement = document.createElement("div");
 scrollElement.classList.add("scrollTop-style", "arrow-down-translate");
-scrollElement.innerHTML = `<i class="fa-solid fa-arrow-up" id="scrollFloatingButton"></i>`;
+scrollElement.innerHTML = `<a href="#"><i class="fa-solid fa-arrow-up" id="scrollFloatingButton"></i><a/>`;
 
 footerElement.after(scrollElement);
-
-const scrollTop = () => {
-    headerSection.scrollIntoView({ behavior: "smooth" });
-};
-
-// const scrollToAbout = () => {
-//     aboutSection.scrollIntoView({ behavior: "smooth" });
-// };
-
-// scrollElement.addEventListener("click", scrollToAbout);
-scrollElement.addEventListener("click", scrollTop);
-
-// window.addEventListener("scroll", () => {
-//     const scrollPosition = window.scrollY;
-//     const heroHeight = heroSection.offsetHeight;
-
-//     const scrollFloatingButton = document.getElementById(
-//         "scrollFloatingButton"
-//     );
-
-//     if (scrollPosition < heroHeight) {
-//         scrollFloatingButton.classList.remove("fa-arrow-up");
-//         scrollFloatingButton.classList.add("fa-arrow-down");
-//         scrollElement.classList.add("arrow-down-translate");
-//         scrollElement.addEventListener("click", scrollToAbout);
-//     } else {
-//         scrollFloatingButton.classList.remove("fa-arrow-down");
-//         scrollElement.classList.remove("arrow-down-translate");
-
-//         scrollFloatingButton.classList.add("fa-arrow-up");
-//         scrollElement.addEventListener("click", scrollTop);
-//     }
-// });
 //* SCROLL TO TOP BUTTON end
 
 //* ACTIVE CLASS RESPONSIVE NAV start
@@ -73,3 +40,20 @@ navListButtons.forEach((navListButton) => {
 });
 
 //* ACTIVE CLASS RESPONSIVE NAV end
+
+//* STICK NAVBAR start
+window.onscroll = () => {
+    const header = document.querySelector(".header");
+
+    header.classList.toggle("sticky", window.scrollY > 50);
+};
+
+// window.addEventListener("scroll", () => {
+//     const header = document.querySelector(".header");
+//     if (window.scrollY > 50) {
+//         header.classList.add("sticky");
+//     } else {
+//         header.classList.remove("sticky");
+//     }
+// });
+//* STICK NAVBAR end
